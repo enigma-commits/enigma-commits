1ae974ee18273ee41f583137e750ee533bd3bbee
Akhilesh Singh Bhadauriya <akhileshsingh2314@gmail.com>
2026-04-01 15:50:05 +0530

    Connect AI proxy URL and lock CORS to portfolio domain

--- worker.js
+++ worker.js
@@ -15,7 +15,7 @@
 export default {
   async fetch(request, env) {
     const corsHeaders = {
-      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Origin': 'https://enigma-commits.github.io',
       'Access-Control-Allow-Methods': 'POST, OPTIONS',
       'Access-Control-Allow-Headers': 'Content-Type',
     };
